a1ee2c4e186b793d17cb60f7963aeeba9b81955b
Celr <120799034+DexArbitrageScript@users.noreply.github.com>
2023-06-23 14:43:18 +0800

    Create unitConverter.ts

---
+++ src/units/unitConverter.ts
@@ -0,0 +1,79 @@
+import { BigNumber } from 'bignumber.js';
+
+BigNumber.set({ DECIMAL_PLACES: 30 });
+
+const units = {
+  wei: '1',
+  kwei: '1000',
+  Kwei: '1000',
+  babbage: '1000',
+  femtoether: '1000',
+  mwei: '1000000',
+  Mwei: '1000000',
+  lovelace: '1000000',
+  picoether: '1000000',
+  gwei: '1000000000',
+  Gwei: '1000000000',
+  shannon: '1000000000',
+  nanoether: '1000000000',
+  nano: '1000000000',
+  szabo: '1000000000000',
+  microether: '1000000000000',
+  micro: '1000000000000',
+  finney: '1000000000000000',
+  milliether: '1000000000000000',
+  milli: '1000000000000000',
+  ether: '1000000000000000000',
+  eth: '1000000000000000000',
+  kether: '1000000000000000000000',
+  grand: '1000000000000000000000',
+  mether: '1000000000000000000000000',
+  gether: '1000000000000000000000000000',
+  tether: '1000000000000000000000000000000',
+};
+
+function convertUnits(value: string, fromUnit: keyof typeof units) {
+  const result: Record<keyof typeof units, string> = {
+        wei: "",
+        kwei: "",
+        Kwei: "",
+        babbage: "",
+        femtoether: "",
+        mwei: "",
+        Mwei: "",
+        lovelace: "",
+        picoether: "",
+        gwei: "",
+        Gwei: "",
+        shannon: "",
+        nanoether: "",
+        nano: "",
+        szabo: "",
+        microether: "",
+        micro: "",
+        finney: "",
+        milliether: "",
+        milli: "",
+        ether: "",
+        eth: "",
+        kether: "",
+        grand: "",
+        mether: "",
+        gether: "",
+        tether: ""
+    };
+
+    result[fromUnit as keyof typeof units] = value;
+    Object.entries(units).forEach(([unit, unitValue]) => {
+        if (unit !== fromUnit) {
+            const dividedValue = new BigNumber(value, 10)
+                .multipliedBy(units[fromUnit])
+                .decimalPlaces(0, BigNumber.ROUND_DOWN)
+                .div(unitValue)
+                .toString(10);
+            
+            result[unit as keyof typeof units] = dividedValue;
+        }
+      });
+  return result;
+}
